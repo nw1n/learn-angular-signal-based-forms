42eae12ad1fbc54fd6edfc234bdcfd30599fa559
Nicolas Wanner <n.wanner@elderbyte.com>
2024-08-24 23:37:33 +0200

    clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,111 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component, computed, effect, signal, ViewChild } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, CommonModule],
+  styles: [
+    `
+      #visual-indicator-el {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+      }
+      #visual-indicator-el.active {
+        background-color: green;
+      }
+      main {
+        padding: 10px;
+      }
+    `,
+  ],
+  template: `
+    <main>
+      <h1>Learn Angular signal based forms</h1>
+      <mat-form-field>
+        <input
+          matInput
+          placeholder="First Name"
+          #firstNameControl="ngModel"
+          [ngModel]="firstNameUc()"
+          (ngModelChange)="setFirstName($event, firstNameControl)"
+          required
+        />
+        <mat-error *ngIf="firstNameControl.hasError('required')">
+          This field is required
+        </mat-error>
+      </mat-form-field>
+      <mat-form-field>
+        <input
+          matInput
+          placeholder="Last Name"
+          #lastNameControl="ngModel"
+          [ngModel]="lastNameUc()"
+          (ngModelChange)="setLastName($event, lastNameControl)"
+          minlength="4"
+        />
+        <mat-error *ngIf="lastNameControl.hasError('minlength')">
+          This field must be at least 4 characters
+        </mat-error>
+      </mat-form-field>
+
+      <div id="visual-indicator-el" #visualIndicatorEl></div>
+
+      <h3>Full Name: {{ fullName() }}</h3>
+</main>
+  `,
 })
 export class AppComponent {
-  title = 'learn-forms';
+  @ViewChild("visualIndicatorEl") visualIndicatorEl: any;
+
+  firstName = signal("");
+  lastName = signal("");
+  fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
+
+  firstNameUc = computed(() => this.firstName().toUpperCase());
+  lastNameUc = computed(() => this.lastName().toUpperCase());
+
+  setFirstName(value: string, control: any) {
+    const val = value.toLowerCase();
+    console.log("setFirstName", val);
+    this.firstName.set(val);
+  }
+  setLastName(value: string, control: any) {
+    const val = value.toLowerCase();
+    console.log("setLastName", val);
+    // Only update when the input is valid
+    if (control.hasError("minlength")) {
+      console.log("minlength error");
+      return;
+    }
+    this.lastName.set(val);
+  }
+
+  toggleVisualEffect() {
+    // it seems necessary to use nativeElement and not change state of component
+    const el = this.visualIndicatorEl.nativeElement;
+    if (el.classList.contains("active")) {
+      el.classList.remove("active");
+    } else {
+      el.classList.add("active");
+    }
+  }
+
+  constructor() {
+    // Effects are used here just to demonstrate triggering side effects on state changes.
+    // E.G. manually changing the background color of a dom element. Other example use cases for effects would be writing to local storage api etc.
+    effect(() => {
+      console.log(`Valid input. The current firstname is: ${this.firstName()}`);
+      this.toggleVisualEffect();
+    });
+    effect(() => {
+      // Note: This effect will only run when the input is valid, since it depends on the lastName signal being set
+      console.log(`Valid input. The current lastname is: ${this.lastName()}`);
+      this.toggleVisualEffect();
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-    }
 ];
